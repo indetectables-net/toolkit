fdae86f33d7ecb152fff98c44c0077919d2d0907
DSR! <xchwarze@gmail.com>
2022-12-27 02:19:00 -0300

    IDR updater incomplete

--- bin/updater/scripts/IDR.bat
+++ bin/updater/scripts/IDR.bat
@@ -1,10 +1,25 @@
 @ECHO OFF
 echo Fix IDR folder structure
+
 cd "updates\master\IDR-master"
+
 del *.cpp
 del *.dfm
 del *.h
 del *.rc
 del *.res
 del *.manifest
+del *.dll
+del *.ini
+del *.txt
+del *.png
+del *.jpg
+del .gitignore
+
 move bin\Idr.exe Idr.exe
+move bin\dis.dll dis.dll
+move bin\Icons.dll Icons.dll
+
+rmdir /s /q bin
+rmdir /s /q obj
+rmdir /s /q Plugins
