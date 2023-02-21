7bced984a8024e02ae29c2fe576df060ae7d442d
DSR! <xchwarze@gmail.com>
2023-02-21 03:49:15 -0300

    Add auto updater code

--- bin/updater/scripts/Toolkit-Updater.bat
+++ bin/updater/scripts/Toolkit-Updater.bat
@@ -8,9 +8,13 @@ taskkill /IM updater.exe /F
 xcopy /Y /C updates\main\toolkit-updater-main .
 xcopy /Y /C updates\main\toolkit-updater-main\bin bin 
 xcopy /Y /C updates\main\toolkit-updater-main\scripts scripts
+xcopy /Y /C updates\main\toolkit-updater-main\migrations migrations
+
+:: Configure installed tools
+::..\auto-config-tools\auto-config-tools.exe /FOLDER=..\updater
 
 :: Run migrations
-bin\hpatchz.exe -f tools.ini migrations\2022.12.1.diff tools.ini
+bin\patch.exe -f tools.ini migrations\2022.12.1.diff
 
 :: Restart updater
 cmd /c updater.exe
