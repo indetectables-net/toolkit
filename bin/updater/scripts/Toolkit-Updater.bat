244ee3d7adb947377f3c042cdbe54e3774e9316f
DSR! <xchwarze@gmail.com>
2023-02-21 04:05:19 -0300

    Cleanup autoupdater code and files

--- bin/updater/scripts/Toolkit-Updater.bat
+++ bin/updater/scripts/Toolkit-Updater.bat
@@ -11,10 +11,7 @@ xcopy /Y /C updates\main\toolkit-updater-main\scripts scripts
 xcopy /Y /C updates\main\toolkit-updater-main\migrations migrations
 
 :: Configure installed tools
-::..\auto-config-tools\auto-config-tools.exe /FOLDER=..\updater
-
-:: Run migrations
-bin\patch.exe -f tools.ini migrations\2022.12.1.diff
+..\auto-config-tools\auto-config-tools.exe /FOLDER=..\updater
 
 :: Restart updater
 cmd /c updater.exe
