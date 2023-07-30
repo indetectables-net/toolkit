1bc0010a2825d8d34a5bb401516f4b7d5161bfca
DSR! <xchwarze@gmail.com>
2023-07-30 07:34:01 -0300

    Update all tools

--- bin/updater/scripts/IDR.bat
+++ bin/updater/scripts/IDR.bat
@@ -1,5 +1,11 @@
 @ECHO OFF
-echo Fix IDR folder structure
+echo Fix IDR folder structure and files
+echo.
+echo Warning: 
+echo The complete list of "Knowledge bases" weighs more than one gigabyte!
+echo so I decided to leave in the pack only the most common ones to use
+echo if you need to use all of them install them from the repo!
+echo.
 
 cd "updates\master\IDR-master"
 
@@ -15,6 +21,7 @@ del *.txt
 del *.png
 del *.jpg
 del .gitignore
+del *.7z
 
 move bin\Idr.exe Idr.exe
 move bin\dis.dll dis.dll
